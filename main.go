bf4d45add4d252907b35de827577a4dd09c2307e
Prakash Sewda <psewda1@gmail.com>
2020-04-11 02:00:22 +0530

    up

--- main.go
+++ main.go
@@ -6,9 +6,21 @@ import (
 	"github.com/rs/xid"
 )
 
+type emp struct {
+	Name string
+	name string
+}
+
 func main() {
 
 	uid := xid.New().String()
 
 	fmt.Println("Hello Math => " + uid)
+
+	e := emp{
+		Name: "N",
+		name: "v",
+	}
+
+	fmt.Println(e)
 }
--- number/number.go
+++ number/number.go
@@ -1,9 +1,21 @@
 package number
 
+import "fmt"
+
+type emp struct {
+	Name string
+	name string
+}
+
 func Sum(num1, num2 int) int {
 	return num1 + num2
 }
 
 func Multiply(num1, num2 int) int {
 	return num1 * num2
+}
+
+func Print() {
+	e := emp{}
+	fmt.Println(e)
 }
